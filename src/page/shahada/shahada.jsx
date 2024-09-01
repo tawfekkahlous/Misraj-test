2bb853b4361899c051d436ed7c27ca79bc7df088
obadaa-kahlous <obadaakahlous@gmail.com>
2024-09-01 22:52:43 +0300

    test

--- src/page/shahada/shahada.jsx
+++ src/page/shahada/shahada.jsx
@@ -1,4 +1,4 @@
-import  { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 import listen from "../../assets/listening.png";
 import talk from "../../assets/talk.png";
 import wrong from "../../assets/wrong.png";
